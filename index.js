0184a8d57d26148c8313fa989edd2ce3d0e10c5c
Codester291 <Codester291@gmail.com>
2019-06-17 23:52:22 +0100

    first Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// import {express} from 'express';
+// import {http} from 'http';
+
+// const server = http.createServer((req, res) => {
+//     if(req.url('/')){
+        
+//     }
+//     if(req.url('/api/v3/courses')){
+
+//     }
+// })
+
+// server.listen(3000);
